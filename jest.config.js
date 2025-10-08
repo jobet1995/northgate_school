ddf91fb44aee4101e0bb3cb45cb0e0c368a94491
jobet1995 <jobetcasquejo221@gmail.com>
2025-10-08 14:10:30 +0800

    fixed

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,24 @@
 /** @type {import('jest').Config} */
 const config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["@babel/preset-env", { targets: { node: "current" } }],
+          ["@babel/preset-react", { runtime: "automatic" }],
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 };
 
 module.exports = config;
